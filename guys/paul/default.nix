3384f8c90fd530e8918aba94549d35f94b7e9560
Snoilt <paul@oellers.net>
2026-05-04 15:30:08 +0200

    feat: add pub keys and update casks

--- guys/paul/default.nix
+++ guys/paul/default.nix
@@ -16,7 +16,11 @@
         userEmail = "paul@oellers.net";
       };
       ssh.authorizedKeys = [
-        # todo
+        #m4-pro
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwq7BEz4SXNwz5gAt9mtmNz6zGV/lJ8b9CUtxS/X7yb paul@oellers.net"
+
+        #m2-air
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAANYKheY8aMzH8VDM2TbInbXgnAKU71TncoAFor0vwE paul@oellers.net"
       ];
     }
 
@@ -29,22 +33,26 @@
         casks = [
           "aerospace"
           "audacity"
+          "anaconda"
           "bambu-studio"
           "battery-toolkit"
           "betterdisplay"
           "blender"
           "codex"
+          "codex-app"
           "dbeaver-community"
           "discord"
           "displaylink"
           "docker-desktop"
           "finicky"
+          "google-chrome"
           "hoppscotch"
           "iterm2"
           "jordanbaird-ice@beta"
           "keepingyouawake"
           "leader-key"
           "mullvad-vpn"
+          "miniconda"
           "obs"
           "obsidian"
           "prismlauncher"
